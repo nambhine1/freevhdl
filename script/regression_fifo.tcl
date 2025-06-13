d52138e93f578cfd66a3fd92bc33494836fcba5f
Nambinina Rakotojaona <53557693+nambhine1@users.noreply.github.com>
2025-06-13 20:23:23 +0200

    Update regression_fifo.tcl

--- script/regression_fifo.tcl
+++ script/regression_fifo.tcl
@@ -1,42 +1,17 @@
-# Get the directory where this script resides
+# Get directory of this Tcl script
 set script_dir [file dirname [info script]]
 
-# Print the script directory for debug
-puts "Script dir: $script_dir"
+# Debug print
+puts "Script directory is: $script_dir"
 
-# Construct and normalize the full path to StartUp.tcl
+# Build absolute path to StartUp.tcl relative to script directory
 set startup_path [file normalize [file join $script_dir ../src/library/OsvvmLibraries/Scripts/StartUp.tcl]]
 
-# Print the resolved full path to StartUp.tcl
-puts "Trying to source: $startup_path"
-
-# Source the StartUp.tcl script
+puts "Trying to source $startup_path"
 source $startup_path
 
-variable OMIT_XILINX_FILES 0
-
+# Your remaining build commands with file join for safety
 build [file join $script_dir ../src/library/OsvvmLibraries/OsvvmLibraries.pro]
-
-if {$::osvvm::ToolName eq "GHDL"} {
-    set OMIT_XILINX_FILES 1
-    SetExtendedAnalyzeOptions {-frelaxed -Wno-specs}
-    SetExtendedSimulateOptions {-frelaxed -Wno-specs -Wno-binding}
-    SetExtendedRunOptions {--ieee-asserts=disable-at-0}
-}
-
-if {$::osvvm::ToolName eq "RivieraPRO"} {
-    set OMIT_XILINX_FILES 1
-    puts "Simulator = $::osvvm::ToolName, OMIT_XILINX_FILES = $OMIT_XILINX_FILES"
-    LinkLibraryDirectory {temp/VHDL_LIBS}
-}
-
-if {$::osvvm::ToolName eq "QuestaSim"} {
-    set OMIT_XILINX_FILES 0
-    SetVHDLVersion 2008
-    vmap unisim "$PrecompiledVivadoIPCores/unisim"
-    vmap xpm "$PrecompiledVivadoIPCores/xpm"
-}
-
 build [file join $script_dir ../src/library/math_utils.pro]
 build [file join $script_dir ../src/axi/src.pro]
 build [file join $script_dir ../tb/osvvm/Axi_stream_fifo/TestHarness_fifo.pro]
