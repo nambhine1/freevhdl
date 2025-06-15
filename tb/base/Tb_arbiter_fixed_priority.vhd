4c15a52023ccd8166f913c879b405d31020fec3d
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-06-15 13:22:16 +0200

    fixed error in checking.

--- tb/base/Tb_arbiter_fixed_priority.vhd
+++ tb/base/Tb_arbiter_fixed_priority.vhd
@@ -50,37 +50,37 @@ begin
 
             request <= "000";
             wait for 10 ns;
-            check_equal(grant, "000", "Grant should be none when no requests");
+            check_equal(grant, 2#000#, "Grant should be none when no requests");
             check_equal(valid_grant, '0', "Valid_grant should be 0 when no requests");
 
         elsif run("Request_0") then
             request <= "001";
             wait for 10 ns;
-            check_equal(grant, "001", "Grant should be request(0)");
+            check_equal(grant, 2#001#, "Grant should be request(0)");
             check_equal(valid_grant, '1', "Valid_grant should be 1");
 
         elsif run("Request_1") then
             request <= "010";
             wait for 10 ns;
-            check_equal(grant, "010", "Grant should be request(1)");
+            check_equal(grant, 2#010#, "Grant should be request(1)");
             check_equal(valid_grant, '1');
 
         elsif run("Request_2") then
             request <= "100";
             wait for 10 ns;
-            check_equal(grant, "100", "Grant should be request(2)");
+            check_equal(grant, 2#100#, "Grant should be request(2)");
             check_equal(valid_grant, '1');
 
         elsif run("Request_0_and_2") then
             request <= "101";
             wait for 10 ns;
-            check_equal(grant, "001", "Grant should be request(0) due to priority");
+            check_equal(grant, 2#001#, "Grant should be request(0) due to priority");
             check_equal(valid_grant, '1');
 
         elsif run("All_Requests") then
             request <= "111";
             wait for 10 ns;
-            check_equal(grant, "001", "Grant should be request(0) due to highest priority");
+            check_equal(grant, 2#001#, "Grant should be request(0) due to highest priority");
             check_equal(valid_grant, '1');
 
         elsif run("Reset_Test") then
@@ -91,7 +91,7 @@ begin
             wait for 10 ns;
             rst <= '1';
             wait for 10 ns;
-            check_equal(grant, "000", "Grant should be cleared on reset");
+            check_equal(grant, 2#000#, "Grant should be cleared on reset");
             check_equal(valid_grant, '0', "Valid_grant should be 0 after reset");
 
         end if;
