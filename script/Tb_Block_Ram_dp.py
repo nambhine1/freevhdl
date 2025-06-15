a98d183843318bbca28a65b1847b9328b6d72754
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-06-15 15:59:43 +0200

    add library in the source  inside script.

--- script/Tb_Block_Ram_dp.py
+++ script/Tb_Block_Ram_dp.py
@@ -8,6 +8,8 @@ lib = vu.add_library("lib")
 
 # Add source files
 lib.add_source_files("src/base/Block_Ram_dp.vhd")
+lib.add_source_files("src/library/math_utils.vhd")
+
 
 # Add testbench files
 lib.add_source_files("tb/base/Tb_Block_Ram_dp.vhd")
