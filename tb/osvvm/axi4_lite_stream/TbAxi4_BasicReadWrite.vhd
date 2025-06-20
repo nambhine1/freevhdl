32e1938b2bd26dd1e004ae888da514e0b30ca35d
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-06-20 09:01:05 +0200

    update ref: add all test cases to check when the data is passthrough, and doing image inversions.

--- tb/osvvm/axi4_lite_stream/TbAxi4_BasicReadWrite.vhd
+++ tb/osvvm/axi4_lite_stream/TbAxi4_BasicReadWrite.vhd
@@ -1,8 +1,9 @@
 architecture BasicReadWrite of TestCtrl is
   use    osvvm.ScoreboardPkg_slv.all;
   signal TestDone : integer_barrier := 1 ;
-  signal Req_1 : AlertLogIDType;
+  signal Req_1, Req_2 : AlertLogIDType;
   signal SB : ScoreboardIDType;
+  signal send_inv_image : std_logic := '0';
 
 begin
   ------------------------------------------------------------
@@ -18,7 +19,7 @@ begin
     SetLogEnable(INFO, FALSE);      -- Enable INFO logs
 	
 	Req_1 <= GetReqID("PR-0001", PassedGoal => 1, ParentID => REQUIREMENT_ALERTLOG_ID);
- 
+    Req_2 <= GetReqID("PR-0002", PassedGoal => 1, ParentID => REQUIREMENT_ALERTLOG_ID);
     -- Wait for Design Reset
     wait until nReset = '1';
 	SB <= NEWID ("Score_Board"); 
@@ -31,7 +32,7 @@ begin
     AlertIf(GetAffirmCount < 500, "Test is not Self-Checking");
 	
 	AffirmIf(Req_1, GetAlertCount = 0, GetTestName & "REQUIREMENT Req_1 FAILED!!!!!") ;
-
+    AffirmIf(Req_2, GetAlertCount = 0, GetTestName & "REQUIREMENT Req_2 FAILED!!!!!") ;
     wait for 1 us;
 
     EndOfTestReports(ReportAll => TRUE);
@@ -69,6 +70,13 @@ begin
       AffirmIfEqual(Data, expect_data, "Manager Read Data: ");
       wait for 10 ns; -- Wait for 10 ns between values
     end loop;
+	
+	send_inv_image <= '1';
+	valu := std_logic_vector(to_unsigned(0, AXI_ADDR_WIDTH));
+    data_send := std_logic_vector(to_unsigned(1, AXI_DATA_WIDTH));
+    Write(ManagerRec, valu*4, data_send);
+	
+	
 
     WaitForClock(ManagerRec, 2);
     WaitForBarrier(TestDone);
@@ -84,6 +92,9 @@ begin
     AxiTransmitterProc : process
 		variable rv : RandomPType;
 		variable rand_data : std_logic_vector (AXI_DATA_WIDTH - 1 downto 0);
+	    variable TxData  : std_logic_vector(AXI_DATA_WIDTH - 1 downto 0);
+		variable Inv_data : std_logic_vector(AXI_DATA_WIDTH - 1 downto 0);
+		variable pixel_val : unsigned(7 downto 0);
 	begin
 		wait until nReset = '1';
 		WaitForClock(StreamTxRec, 2);
@@ -96,6 +107,25 @@ begin
 			rand_data := x"00000001";  -- match DATA_WIDTH
 			Push(SB, rand_data);
 			Send(StreamTxRec, rand_data);
+		end loop;
+		
+		wait until send_inv_image = '1';
+		
+		WaitForClock(StreamTxRec, 25);
+	    TxData := (others => '0');  -- Start from 0
+	
+		for J in 0 to 500 loop
+			-- Process each 8-bit lane of the 32-bit data word
+			for i in 0 to 3 loop
+				pixel_val := unsigned(TxData((i+1)*8 - 1 downto i*8));
+				Inv_data((i+1)*8 - 1 downto i*8) := std_logic_vector(to_unsigned(255 - to_integer(pixel_val), 8));
+			end loop;
+	
+			Push(SB, Inv_data);       -- Expected inverted data
+			Send(StreamTxRec, TxData); -- Send original data
+	
+			-- Optional: increment TxData (to simulate incrementing pixel pattern)
+			TxData := std_logic_vector(unsigned(TxData) + 1);
 		end loop;
 	
 		WaitForClock(StreamTxRec, 2);
@@ -118,6 +148,12 @@ begin
 	
 	ExpData := (others => '0');
 	for J in 0 to 999 loop
+		Get(StreamRxRec, RcvData);
+		log("Data Received: " & to_hstring(RcvData), Level => DEBUG);
+		Check(SB,RcvData);
+	end loop;
+	
+    for J in 0 to 500 loop
 		Get(StreamRxRec, RcvData);
 		log("Data Received: " & to_hstring(RcvData), Level => DEBUG);
 		Check(SB,RcvData);
