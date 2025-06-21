106d352e1e65d182de613eea46030020a8bd89a4
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-06-21 13:11:03 +0200

    update ref: add more functions in the math lib : binary to gray and reverse

--- tb/library/Tb_math_utils.vhd
+++ tb/library/Tb_math_utils.vhd
@@ -14,6 +14,7 @@ end entity;
 architecture tb of tb_math_utils is
 begin
   main : process
+    variable runner : runner_t;
   begin
     test_runner_setup(runner, runner_cfg);
 
@@ -34,9 +35,10 @@ begin
       check_equal(max_value(-10, -5), -5, "max_value(-10,-5)");
       check_equal(max_value(0, 0), 0, "max_value(0,0)");
 
-    elsif run("Testing min") then
-      check_equal(min_value(5, 10), 5, "min(5,10)");
-      check_equal(min_value(8, 10), 8, "min(8,10)");
+    elsif run("Testing min_value") then
+      check_equal(min_value(5, 10), 5, "min_value(5,10)");
+      check_equal(min_value(8, 10), 8, "min_value(8,10)");
+
     elsif run("Testing ispowerof2") then
       check_true(ispowerof2(1), "ispowerof2(1)");
       check_true(ispowerof2(2), "ispowerof2(2)");
@@ -49,6 +51,55 @@ begin
       check_false(ispowerof2(1023), "ispowerof2(1023)");
       check_false(ispowerof2(-1), "ispowerof2(-1)");
       check_false(ispowerof2(-4), "ispowerof2(-4)");
+
+    elsif run("Test binary to gray") then
+      -- Test values and their expected Gray codes
+      constant test_vals : integer_vector := (0, 1, 2, 3, 4, 7, 15, 31, 255);
+      constant expected_grays : integer_vector := (
+        0, 1, 3, 2, 6, 4, 8, 16, 383
+      );
+      variable bin_vec : std_logic_vector(31 downto 0);
+      variable gray_vec : std_logic_vector(31 downto 0);
+      variable expected_gray_vec : std_logic_vector(31 downto 0);
+
+      for i in test_vals'range loop
+        bin_vec := std_logic_vector(to_unsigned(test_vals(i), 32));
+        expected_gray_vec := std_logic_vector(to_unsigned(expected_grays(i), 32));
+
+        gray_vec := binary_to_gray(bin_vec);
+
+        check_equal(gray_vec, expected_gray_vec,
+          "binary_to_gray failed for input " & integer'image(test_vals(i)));
+
+        -- Round-trip: gray_to_binary(binary_to_gray(x)) = x
+        check_equal(gray_to_binary(gray_vec), bin_vec,
+          "round-trip binary_to_gray->gray_to_binary failed for input " & integer'image(test_vals(i)));
+      end loop;
+
+    elsif run("Test gray to binary") then
+      -- Test values and their Gray codes for gray_to_binary
+      constant gray_vals : integer_vector := (0, 1, 3, 2, 6, 4, 8, 16, 383);
+      constant expected_bins : integer_vector := (
+        0, 1, 2, 3, 4, 7, 15, 31, 255
+      );
+      variable gray_vec : std_logic_vector(31 downto 0);
+      variable bin_vec : std_logic_vector(31 downto 0);
+      variable expected_bin_vec : std_logic_vector(31 downto 0);
+
+      for i in gray_vals'range loop
+        gray_vec := std_logic_vector(to_unsigned(gray_vals(i), 32));
+        expected_bin_vec := std_logic_vector(to_unsigned(expected_bins(i), 32));
+
+        bin_vec := gray_to_binary(gray_vec);
+
+        check_equal(bin_vec, expected_bin_vec,
+          "gray_to_binary failed for input " & integer'image(gray_vals(i)));
+
+        -- Round-trip: binary_to_gray(gray_to_binary(x)) = x
+        check_equal(binary_to_gray(bin_vec), gray_vec,
+          "round-trip gray_to_binary->binary_to_gray failed for input " & integer'image(gray_vals(i)));
+      end loop;
+
     end if;
 
     test_runner_cleanup(runner);
