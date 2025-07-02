9aac8a9a9b69229e6d3133ddf9665541da9ace33
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-07-02 20:47:31 +0200

    point to max value for simulation not min

--- script/Tb_max_value.py
+++ script/Tb_max_value.py
@@ -8,11 +8,11 @@ lib = vu.add_library("lib")
 
 # Add source files
 lib.add_source_files("src/library/math_utils.vhd")
-lib.add_source_files("src/base/min_value.vhd")
+lib.add_source_files("src/base/max_value.vhd")
 
 
 
 # Add testbench files
-lib.add_source_files("tb/base/Tb_min_value.vhd")
+lib.add_source_files("tb/base/Tb_max_value.vhd")
 # Run VUnit
 vu.main()
