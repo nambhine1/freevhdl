d6ff686ddfaa12ebe370fd2940e81f7fe699132e
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-07-07 20:10:35 +0200

    update test control

--- tb/osvvm/Axi_stream_byte_packaging/TestCtrl_e.vhd
+++ tb/osvvm/Axi_stream_byte_packaging/TestCtrl_e.vhd
@@ -72,7 +72,7 @@ entity TestCtrl is
   -- Derive AXI interface properties from the StreamTxRec
   constant DATA_WIDTH_TRANS : integer := StreamTxRec.DataToModel'length ; 
   constant DATA_WIDTH_RECEIV : integer := DATA_WIDTH_TRANS *3;
-  constant DATA_BYTES : integer := DATA_WIDTH/8 ; 
+  constant DATA_BYTES : integer := 4 ; 
   -- Simplifying access to Burst FIFOs using aliases
   alias TxBurstFifo : ScoreboardIdType is StreamTxRec.BurstFifo ; 
   alias RxBurstFifo : ScoreboardIdType is StreamRxRec.BurstFifo ;
